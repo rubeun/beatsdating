72ffca5873d81c65e99b5371380520ee4c43cf22
Rubeun Tan <rubeun@Rubeuns-MacBook-Pro.local>
2024-01-19 02:47:34 -0800

    add zipcode API utility function

---
+++ src/api/zipcode.js
@@ -0,0 +1,11 @@
+/**
+ * Returns an array of objects with zipcodes within the radius of a zipcode
+ * @param {number} zipcode 
+ * @param {number} radius 
+ */
+const zipcodesWithinRange = (zipcode, radius) => {
+  const zipcodeAPIKey = process.env.REACT_APP_ZIPCODE_API_KEY;
+
+}
+ 
+export default zipcodesWithinRange;
